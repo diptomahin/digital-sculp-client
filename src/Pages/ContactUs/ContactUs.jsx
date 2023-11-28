2df7011c420a9513548618f88bddb89a5ed49b04
Mahin Ahmed <mahindipto18@gmail.com>
2023-11-28 15:50:58 +0600

    added contact page

--- src/Pages/ContactUs/ContactUs.jsx
+++ src/Pages/ContactUs/ContactUs.jsx
@@ -1,8 +1,46 @@
+import { Label, TextInput } from 'flowbite-react';
+import Banner from '../Home/HomeComonents/Banner';
 
+import { Button } from 'flowbite-react';
 const ContactUs = () => {
     return (
         <div>
-            <h1>Contact Us</h1>
+            <Banner></Banner>
+            <div className='w-11/12 mx-auto my-20   bg-[#D4F6C8] p-10 rounded-lg'>
+                <h1 className="text-3xl text-center mb-5 font-bold border-[#FFE074] border-x-2 py-5 bg-[#D4F6C8] border-y-4">Contact US</h1>
+                <div className="flex max-w-md flex-col gap-4 mx-auto" >
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="small" value="Your Name" />
+                        </div>
+                        <TextInput id="small" type="text" sizing="sm" />
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="base" value="Your Email" />
+                        </div>
+                        <TextInput id="base" type="text" sizing="md" />
+                    </div>
+                    <div>
+                        <div className="mb-2 block">
+                            <Label htmlFor="large" value="Query" />
+                        </div>
+                        <TextInput id="large" type="text" sizing="lg" />
+                    </div>
+                </div>
+                <Button color="info" className='mx-auto mt-4'>Submit</Button>
+            </div>
+            <div className='w-11/12 mx-auto my-20   bg-[#D4F6C8] p-10 rounded-lg'>
+                <h1 className="text-3xl text-center mb-5 font-bold border-[#FFE074] border-x-2 py-5 bg-[#D4F6C8] border-y-4">Address</h1>
+                <p className='text-2xl font-semibold text-center'>
+                    email: digital@conatact.com <br />
+                    phone:+440264856 <br />
+                    DigitalSculpt Marketing <br />
+                    123 Digital Avenue <br />
+                    Cityville, State 56789 <br />
+                    Country <br />
+                    </p>
+            </div>
         </div>
     );
 };
